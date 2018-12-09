c49b0bbd5c5fd7d4714e44f80d359fe43fcd8902
gulu <guangyi.lu@polycom.com>
2018-12-09 17:01:42 +0800

    add leet

---
+++ leet/0001-two-sum/two-sum.go
@@ -0,0 +1,21 @@
+func twoSum(nums []int, target int) []int {
+    
+    if nums == nil || len(nums) == 0 {
+        return nil
+    }
+    
+    var numMap = map[int]int{}
+    
+    for i, val := range nums {
+        
+        n, ok := numMap[ target - val ]
+        
+        if(ok){
+            return []int{n, i}
+        }else{
+            numMap[val] = i
+        }
+    }
+    
+    return nil
+}

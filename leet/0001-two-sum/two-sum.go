9af671153bdac454ff4cde91ab38d1fc1f5b06c2
gulu <guangyi.lu@polycom.com>
2018-12-09 17:04:18 +0800

    fix typo

--- leet/0001-two-sum/two-sum.go
+++ leet/0001-two-sum/two-sum.go
@@ -1,15 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    
     if nums == nil || len(nums) == 0 {
         return nil
     }
     
     var numMap = map[int]int{}
-    
     for i, val := range nums {
-        
         n, ok := numMap[ target - val ]
-        
         if(ok){
             return []int{n, i}
         }else{

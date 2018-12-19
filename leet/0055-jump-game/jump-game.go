6e0afcbda28e23f683a4e2a8e50ebeff05fbae11
lugy-bupt <lugy_bupt@126.com>
2018-12-19 15:18:04 +0800

    jump game

---
+++ leet/0055-jump-game/jump-game.go
@@ -0,0 +1,26 @@
+func canJump(nums []int) bool {
+    if len(nums) < 2 {return true}
+	if nums[0] == 0 {return false}
+	
+	result := true
+	lastIndex := 0
+	for i, num := range nums {
+        if num == 0 && i < len(nums) - 1{
+			singleResult := false
+			for j := i - 1; j >= lastIndex; j-- {
+				step := i - j
+				if nums[j] > step { // true 
+					singleResult = true
+					break;
+				}
+			}
+			
+			result = result && singleResult
+			
+			if (!result){
+				return result
+			}
+		}
+	}
+	return result
+}

8aafa5fbf9f915603d751c7f5b754e175c1b5ee7
lugy-bupt <lugy_bupt@126.com>
2019-03-18 20:02:44 +0800

    Create subarray-sums-divisible-by-k.py

---
+++ leet/0974-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraysDivByK(self, A: List[int], K: int) -> int:
+        pre_sum, res = 0, 0
+        dic = {0:1}
+        
+        for val in A:
+            pre_sum = (pre_sum + val) % K
+            if pre_sum in dic:
+                res += dic[pre_sum]
+            dic[pre_sum] = dic.get(pre_sum, 0) + 1
+                
+        return res 
+            

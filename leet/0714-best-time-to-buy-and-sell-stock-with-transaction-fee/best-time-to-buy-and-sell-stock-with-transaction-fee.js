65a174cc352bb312944928388150cb36a86ec3f0
lugy-bupt <lugy_bupt@126.com>
2019-04-18 18:09:27 +0800

    Create best-time-to-buy-and-sell-stock-with-transaction-fee.js

---
+++ leet/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(prices, fee) {
+    let m = prices.length
+    if(m < 2) return 0
+
+    const b = new Array(m)
+    const s = new Array(m)
+
+    b[0] = 0 - prices[0]
+    s[0] = 0
+
+    for(let i = 1; i < m; i++){
+        b[i] = Math.max(b[i - 1], s[i - 1] - prices[i])
+        s[i] = Math.max(s[i - 1], prices[i] + b[i - 1] - fee)
+    }
+
+    return s[m - 1]    
+};

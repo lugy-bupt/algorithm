d05a6567c3d45d070c1a0d47f4b3b0d369738270
lugy-bupt <lugy_bupt@126.com>
2019-04-23 16:02:05 +0800

    Update arithmetic-slices.js

--- leet/0413-arithmetic-slices/arithmetic-slices.js
+++ leet/0413-arithmetic-slices/arithmetic-slices.js
@@ -8,7 +8,6 @@ var numberOfArithmeticSlices = function(A) {
 
     let mp = new Map()
     let ret = 0
-    let temp = 0
     for (let i = 2; i < m; i++){
         let diff0 = A[i] - A[i - 1]
         let diff1 = A[i - 1] - A[i - 2]
@@ -17,20 +16,16 @@ var numberOfArithmeticSlices = function(A) {
             let arr = mp.get(diff1) || new Array(m).fill(0)
             mp.set(diff1, arr)
             let last = i - 1
-            arr[i] = arr[last] + 1
+            arr[i] = 1
             while(arr[last] != 0){
                 arr[i] += 1
                 last--
             }
-            temp = Math.max(temp, arr[i])
-        }else{
-            ret += temp
-            temp = 0
+            ret += arr[i]
         }
     }
-    ret += temp
 
-    console.log(mp)
+    //console.log(mp)
 
     return ret
 };

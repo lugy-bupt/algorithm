7318ea924b83f7b140a832239408aa25bd7f4698
lugy-bupt <lugy_bupt@126.com>
2019-05-22 14:56:53 +0800

    Create jump-game-II_dp.js

---
+++ leet/0045-jump-game-II/jump-game-II_dp.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let mp = new Array(nums.length).fill(99999)
+    mp[0] = 0
+
+    let dp = function(n){
+        let x = nums[n]
+        for (let i = n + 1; i < nums.length && i <= n + x ; i++){
+            mp[i] = Math.min(mp[i], mp[n] + 1)
+        }
+    }
+
+    for(let i = 0; i < nums.length; i++) dp(i)
+    return mp[mp.length - 1]
+};
